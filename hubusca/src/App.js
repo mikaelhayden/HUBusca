98d5c3eae585a2582dbb74809a5c93b8af0fff5b
Mikael Hayden <mikaelhaydencf@gmail.com>
2023-11-29 19:43:54 -0300

    :wrench: feat: conectando a API
    conectei a api do github com o js, agora já é possível fazer a pesquisa por usuário

--- hubusca/src/App.js
+++ hubusca/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [name, setName] = useState("Loading...");
+  const [login, setLogin] = useState("Loading...");
+  const [location, setLocation] = useState("Loading...");
+  const [avatar_url, setAvatarURL] = useState("Loading...");
+
+  const handleSearch = () => {
+    axios.get(`https://api.github.com/users/${search}`)
+.then((res) => {
+        console.log(res.data)
+        setName(res.data.name);
+        setLogin(res.data.login);
+        setLocation(res.data.location);
+        setAvatarURL(res.data.avatar_url);
+      })
+
+      .catch((error) => {
+        console.error("Erro na busca do perfil:", error);
+        // Lidar com o erro
+      });
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-app'>
+      <div className='container'>
+        <header className='header-top'>
+          <ul>
+            <li>Jovem Programador</li>
+          </ul>
+        </header>
+
+        <main>
+          <div className='form'>
+            <h1>Buscador de Perfis do Github</h1>
+            <input type='text' 
+            placeholder='Digite um username' 
+            onChange={(e) => setSearch(e.target.value)}
+            />
+            <button onClick={handleSearch}>Buscar</button>
+          </div>
+          <div className='content'>
+            <div>
+              <img src={avatar_url} alt='Perfil'/>
+              <h1>{name}</h1>
+              <p>{login}</p>
+              <p>{location}</p>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
+
   );
 }
 
